818f71bb2e8924c05cf2e6b1dda22f77f7bfdc3d
nclarius <natalie_clarius@yahoo.de>
2021-11-24 16:56:07 +0100

    update metadata

--- always-open-on-active-screen/contents/code/main.js
+++ always-open-on-active-screen/contents/code/main.js
@@ -13,10 +13,10 @@ workspace.clientAdded.connect(function(client) {
     // clip and move client into bounds of screen dimensions
     if (!client.moveable) return;
     area = workspace.clientArea(KWin.MaximizeArea, client);
-    // window width/height maximally screen width/height
+    // set window width/height to maximally screen width/height
     client.geometry.width = Math.min(client.width, area.width);
     client.geometry.height = Math.min(client.height, area.height);
-    // left/top window edge between left and right/top and bottom screen edges
+    // set left/top window edge to between left and right/top and bottom screen edges
     client.geometry.x = Math.max(area.x, Math.min(area.x + area.width - client.width, client.x));
     client.geometry.y = Math.max(area.y, Math.min(area.y + area.height - client.height, client.y));
 });
